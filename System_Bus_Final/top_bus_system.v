41d9022e04f5182852fcc8f573367889e02058a5
Amoda Attanayake <amodaattanayake@gmail.com>
2026-09-02 02:32:52 +0530

    Modularize system bus

--- System_Bus_Final/top_bus_system.v
+++ System_Bus_Final/top_bus_system.v
@@ -25,28 +25,162 @@ module top_bus_system (
     output wire        ext_tx_serial
 );
 
-    // Instantiate Sub-system Interconnect
+    // Master-Interconnect Signals
+    wire req0, req1, gnt0, gnt1;
+    wire m0_split_notify, m1_split_notify;
+    wire [13:0] m0_addr, m1_addr;
+    wire [7:0]  m0_wdata, m1_wdata;
+    wire        m0_we, m1_we;
+    wire        m0_valid, m1_valid;
+
+    // Shared Interconnect Output Signals
+    wire [13:0] bus_addr;
+    wire [7:0]  bus_wdata;
+    wire        bus_we;
+    wire [7:0]  bus_rdata;
+    wire        bus_ready;
+
+    // Decoder Selects
+    wire sel_s0, sel_s1, sel_s2, sel_bridge;
+
+    // Slave Response Signals
+    wire [7:0] s0_rdata, s1_rdata, s2_rdata, sb_rdata;
+    wire       s0_ready, s1_ready, s2_ready, sb_ready;
+    wire       s0_split_req, s0_split_done;
+
+    // ------------------------------------------------------------------------
+    // Master Nodes
+    // ------------------------------------------------------------------------
+    master_node m0_inst (
+        .clk       (clk),            .rst_n    (rst_n),
+        .cmd_start (m0_cmd_start),   .cmd_we   (m0_cmd_we), 
+        .cmd_addr  (m0_cmd_addr),    .cmd_wdata(m0_cmd_wdata),
+        .cmd_rdata (m0_cmd_rdata),   .cmd_done (m0_cmd_done),
+        .bus_req   (req0),           .bus_gnt  (gnt0), 
+        .bus_split (m0_split_notify),
+        .m_addr    (m0_addr),        .m_wdata  (m0_wdata), 
+        .m_we      (m0_we),          .m_valid  (m0_valid),
+        .bus_rdata (bus_rdata),      .bus_ready(bus_ready)
+    );
+
+    master_node m1_inst (
+        .clk       (clk),            .rst_n    (rst_n),
+        .cmd_start (m1_cmd_start),   .cmd_we   (m1_cmd_we), 
+        .cmd_addr  (m1_cmd_addr),    .cmd_wdata(m1_cmd_wdata),
+        .cmd_rdata (m1_cmd_rdata),   .cmd_done (m1_cmd_done),
+        .bus_req   (req1),           .bus_gnt  (gnt1), 
+        .bus_split (m1_split_notify),
+        .m_addr    (m1_addr),        .m_wdata  (m1_wdata), 
+        .m_we      (m1_we),          .m_valid  (m1_valid),
+        .bus_rdata (bus_rdata),      .bus_ready(bus_ready)
+    );
+
+    // ------------------------------------------------------------------------
+    // System Bus Interconnect
+    // ------------------------------------------------------------------------
     bus_interconnect interconnect_inst (
+        .clk             (clk),
+        .rst_n           (rst_n),
+
+        // Master 0 Interface
+        .req0            (req0),
+        .gnt0            (gnt0),
+        .m0_split_notify (m0_split_notify),
+        .m0_addr         (m0_addr),
+        .m0_wdata        (m0_wdata),
+        .m0_we           (m0_we),
+        .m0_valid        (m0_valid),
+
+        // Master 1 Interface
+        .req1            (req1),
+        .gnt1            (gnt1),
+        .m1_split_notify (m1_split_notify),
+        .m1_addr         (m1_addr),
+        .m1_wdata        (m1_wdata),
+        .m1_we           (m1_we),
+        .m1_valid        (m1_valid),
+
+        // Bus Out to Slaves
+        .bus_addr        (bus_addr),
+        .bus_wdata       (bus_wdata),
+        .bus_we          (bus_we),
+
+        // Decoded Selects
+        .sel_s0          (sel_s0),
+        .sel_s1          (sel_s1),
+        .sel_s2          (sel_s2),
+        .sel_bridge      (sel_bridge),
+
+        // Split Handshakes
+        .s0_split_req    (s0_split_req),
+        .s0_split_done   (s0_split_done),
+
+        // Slave Data Inputs
+        .s0_rdata        (s0_rdata),
+        .s0_ready        (s0_ready),
+        .s1_rdata        (s1_rdata),
+        .s1_ready        (s1_ready),
+        .s2_rdata        (s2_rdata),
+        .s2_ready        (s2_ready),
+        .sb_rdata        (sb_rdata),
+        .sb_ready        (sb_ready),
+
+        // Bus Responses back to Masters
+        .bus_rdata       (bus_rdata),
+        .bus_ready       (bus_ready)
+    );
+
+    // ------------------------------------------------------------------------
+    // Slave Nodes
+    // ------------------------------------------------------------------------
+    // Slave 0 (4K Split RAM)
+    slave_0_split_4k s0_inst (
+        .clk        (clk),
+        .rst_n      (rst_n),
+        .sel        (sel_s0),
+        .we         (bus_we),
+        .addr       (bus_addr[11:0]),
+        .din        (bus_wdata),
+        .dout       (s0_rdata),
+        .ready      (s0_ready),
+        .split_req  (s0_split_req),
+        .split_done (s0_split_done)
+    );
+
+    // Slave 1 (4K Fast RAM)
+    slave_fast_ram #(.ADDR_WIDTH(12)) s1_inst (
+        .clk   (clk),
+        .rst_n (rst_n),
+        .sel   (sel_s1),
+        .we    (bus_we),
+        .addr  (bus_addr[11:0]),
+        .din   (bus_wdata),
+        .dout  (s1_rdata),
+        .ready (s1_ready)
+    );
+
+    // Slave 2 (2K Fast RAM)
+    slave_fast_ram #(.ADDR_WIDTH(11)) s2_inst (
+        .clk   (clk),
+        .rst_n (rst_n),
+        .sel   (sel_s2),
+        .we    (bus_we),
+        .addr  (bus_addr[10:0]),
+        .din   (bus_wdata),
+        .dout  (s2_rdata),
+        .ready (s2_ready)
+    );
+
+    // External Bridge
+    cross_fpga_bridge_dummy bridge_inst (
         .clk           (clk),
         .rst_n         (rst_n),
-
-        // Master 0 Wires
-        .m0_cmd_start (m0_cmd_start),
-        .m0_cmd_we    (m0_cmd_we),
-        .m0_cmd_addr  (m0_cmd_addr),
-        .m0_cmd_wdata (m0_cmd_wdata),
-        .m0_cmd_rdata (m0_cmd_rdata),
-        .m0_cmd_done  (m0_cmd_done),
-
-        // Master 1 Wires
-        .m1_cmd_start (m1_cmd_start),
-        .m1_cmd_we    (m1_cmd_we),
-        .m1_cmd_addr  (m1_cmd_addr),
-        .m1_cmd_wdata (m1_cmd_wdata),
-        .m1_cmd_rdata (m1_cmd_rdata),
-        .m1_cmd_done  (m1_cmd_done),
-
-        // Serial Bridge Wires
+        .sel           (sel_bridge),
+        .we            (bus_we),
+        .addr          (bus_addr[11:0]),
+        .din           (bus_wdata),
+        .dout          (sb_rdata),
+        .ready         (sb_ready),
         .ext_rx_serial (ext_rx_serial),
         .ext_tx_serial (ext_tx_serial)
     );
